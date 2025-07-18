025d1e79d81653e8bc1468467f55132dc4e0356a
Scott Anderson <scottanderson3325@pointloma.edu>
2025-07-18 00:02:56 -0700

    Full commit with new schedule page

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Workout Tracker!"
+    return render_template('home.html')
+
+@app.route('/schedule', methods=['GET', 'POST'])
+def schedule():
+    if request.method == 'POST':
+        workouts = {
+            "Monday": request.form.get("monday"),
+            "Tuesday": request.form.get("tuesday"),
+            "Wednesday": request.form.get("wednesday"),
+            "Thursday": request.form.get("thursday"),
+            "Friday": request.form.get("friday"),
+            "Saturday": request.form.get("saturday"),
+            "Sunday": request.form.get("sunday"),
+        }
+        return render_template("schedule.html", workouts=workouts)
+
+    return render_template("schedule.html", workouts={})
 
 if __name__ == '__main__':
     app.run(debug=True)
